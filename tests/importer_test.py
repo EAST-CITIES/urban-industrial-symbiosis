b89d5159924dc75bf2d734e8e5814517e48c7d12
bolandka <katarina.boland@gesis.org>
2020-01-11 12:11:51 +0100

    fix module path configuration

--- tests/importer_test.py
+++ tests/importer_test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from context import symbiosis
+#from context import symbiosis
+from context import model
+from context import importer
 import os
 
 data_path = os.path.join(os.path.dirname(__file__), "..", "data")
@@ -9,13 +11,13 @@ company_data_path = os.path.join(data_path, "20191216_Unternehmensverzeichnis_To
 association_table_path = os.path.join(data_path, "20191216_Association_Table_2.xlsx")
 
 def test_association_table_import():
-    entries_with_thermal_energy = [entry for entry in symbiosis.importer.import_association_table(association_table_path) if entry.energy.thermal_out]
+    entries_with_thermal_energy = [entry for entry in importer.import_association_table(association_table_path) if entry.energy.thermal_out]
 
     assert(len(entries_with_thermal_energy) == 5)
 
 
 def test_import_data():
-    assoc_table, company_data = symbiosis.importer.import_data(association_table_path, company_data_path)
+    assoc_table, company_data = importer.import_data(association_table_path, company_data_path)
     for company in company_data:
         print(company)
 
